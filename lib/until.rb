dd304fd430a8ecd264e479a989cdc34e71a21c96
Fernando Hidalgo <fernandojesus.hidalgominaya9@gmail.com>
2020-07-09 21:03:20 +0000

    Automatically backed up by Learn

--- lib/until.rb
+++ lib/until.rb
@@ -1,13 +1,8 @@
 def using_until
   levitation_force = 6
   until levitation_force == 10 
- puts "Wingardium Leviosa"
+  puts "Wingardium Leviosa"
   levitation_force += 1 
 end
 end   
-end
-
-def using_while
-  levitation_force = 6
-  while levitation_force < 10 
   
